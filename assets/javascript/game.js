2c215293432039751df97ba0287855d63a388f64
David <DMcCulloch86@gmail.com>
2019-09-23 20:13:56 -0400

    added variables to html and js

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+let wins = 0;
+let currentWord = 'none';
+let guessesRemaining = 10;
+let lettersGuessed = 'A';
+
+
+document.getElementById('wins').innerHTML= wins;
+
+document.getElementById('currentWord').innerHTML = currentWord;
+
+document.getElementById('guessesRemaining').innerHTML= guessesRemaining;
+
+document.getElementById('lettersGuessed').innerHTML= lettersGuessed;
