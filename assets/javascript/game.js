aa399e2f9694dbbad8833d650a12ecfaec12d87f
David <DMcCulloch86@gmail.com>
2019-09-24 16:50:13 -0400

    testing to get code to compate key pressed to the word from word bank

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,34 @@
-let wins = 0;
-let currentWord = 'none';
-let guessesRemaining = 10;
-let lettersGuessed = ['apple', 'grape', 'pear']
-
-document.getElementById('wins').innerHTML= wins;
-document.getElementById('currentWord').innerHTML = currentWord;
-document.getElementById('guessesRemaining').innerHTML= guessesRemaining;
-document.getElementById('lettersGuessed').innerHTML= lettersGuessed;
+let wins = 0; //test
+let currentWord = 'none'; //test
+let guessesRemaining = 10; //test
+let lettersGuessed = ['apple', 'grape', 'pear'] //test
 
 let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 let wordBank = ['pikachu', 'bulbasaur', 'charmander', 'greninja', 'litten', 'vulpix', 'muk', 'jigglypuff', 'magikarp', 'eevee', 'mewtwo']
 
-let word = {
+/*let word = {
     generate: wordBank [Math.floor(Math.random()*wordBank.length)],
     
 
 
-}
+}*/
+let word = wordBank [Math.floor(Math.random()*wordBank.length)];
 
 
 
 document.onkeyup = function(event) {
     let keyPress = String.fromCharCode(event.keyCode).toLowerCase();
-    if (letters.indexOf(keyPress) > 1) {
-        console.log(keyPress); //tester
-        console.log(word.generate); //tester
+    if (letters.indexOf(keyPress) > -1) {
+        console.log(keyPress); //test
+        console.log(word.generate); //test
+
+        //test to compare to key pressed to see if it is in the word
+        if (word.includes(keyPress) > -1) {
+            console.log('yes')
+        } else {
+            console.log('no')
+        }
 
 
     } else {
@@ -35,6 +38,11 @@ document.onkeyup = function(event) {
 
 
 
+
+document.getElementById('wins').innerHTML= wins;
+document.getElementById('currentWord').innerHTML = currentWord;
+document.getElementById('guessesRemaining').innerHTML= guessesRemaining;
+document.getElementById('lettersGuessed').innerHTML= lettersGuessed;
 
 
 
