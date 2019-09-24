cf24186c6ca3da1bdb9ac98b65b9755c46ad73b8
David <DMcCulloch86@gmail.com>
2019-09-24 11:53:43 -0400

    created word bank and started working on the object for the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,28 +3,34 @@ let currentWord = 'none';
 let guessesRemaining = 10;
 let lettersGuessed = ['apple', 'grape', 'pear']
 
-
 document.getElementById('wins').innerHTML= wins;
-
 document.getElementById('currentWord').innerHTML = currentWord;
-
 document.getElementById('guessesRemaining').innerHTML= guessesRemaining;
-
 document.getElementById('lettersGuessed').innerHTML= lettersGuessed;
 
 let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+let wordBank = ['pikachu', 'bulbasaur', 'charmander', 'greninja', 'litten', 'vulpix', 'muk', 'jigglypuff', 'magikarp', 'eevee', 'mewtwo']
+
+let word = {
+
+
+
+
+}
 
 
 document.onkeyup = function(event) {
     let keyPress = String.fromCharCode(event.keyCode).toLowerCase();
     if (letters.indexOf(keyPress) > 1) {
         console.log(keyPress);
+
+
+
     } else {
         alert('Invalid Input')
     }
 }
-
-console.log(letters.length)
 
 
 
