443b22af85714f14d8d64445f27f3ab22695a8c2
David <DMcCulloch86@gmail.com>
2019-09-25 18:13:34 -0400

    split guessed at word into array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,12 +5,11 @@ let lettersGuessed = ['apple', 'grape', 'pear'] //test
 
 let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-let wordBank = ['pikachu', 'bulbasaur', 'charmander', 'greninja', 'litten', 'vulpix', 'muk', 'jigglypuff', 'magikarp', 'eevee', 'mewtwo']
+let wordBank = ['pikachu', 'bulbasaur', 'charmander', 'greninja', 'litten', 'vulpix',
+    'muk', 'jigglypuff', 'magikarp', 'eevee', 'mewtwo']
 
 let word = {
     generate: wordBank [Math.floor(Math.random()*wordBank.length)],
-    
-
 
 }
 
@@ -22,9 +21,13 @@ document.onkeyup = function(event) {
     if (letters.indexOf(keyPress) > -1) {
         console.log(keyPress); //test
         console.log(word.generate); //test
+        //wordArray is now an array of the generated word.
+        let wordArray = word.generate.split(""); 
+        console.log(wordArray)
 
         if (word.generate.includes(keyPress)) {
             console.log('yes') //test
+            
         } else {
             console.log('no') //test
         };
@@ -45,4 +48,4 @@ document.getElementById('lettersGuessed').innerHTML= lettersGuessed;
 
 
 
-
+//add pokemon sound effects and music
