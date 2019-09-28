aef015367ba9a5c6618f478c9ff056e000205e9a
David <DMcCulloch86@gmail.com>
2019-09-28 13:35:12 -0400

    made method to display word as it is being guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,38 @@
 let wins = 0; //test
 let guessesRemaining = 10; //test
-let lettersGuessed = ['apple', 'grape', 'pear']; //test
-let currentWordVisual = 'start';
+let lettersGuessed = []; //test
+let currentWord = 'start';
 
-let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+let unguessedLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 let wordBank = ['pikachu', 'bulbasaur', 'charmander', 'greninja', 'litten', 'vulpix',
     'muk', 'jigglypuff', 'magikarp', 'eevee', 'mewtwo']
 
 let word = {
     generate: wordBank [Math.floor(Math.random()*wordBank.length)],
-    exchange: function(letter, x) {
-        for (let i=0; i < letter.length; i++){
+    delete: function(x) {
+        unguessedLetters.splice(unguessedLetters.indexOf(x),1)
+        },
+    exchange: function(currentGuess, x) {
+
+        let currentWordVisual = '';
+
+        for (let i=0; i < currentGuess.length; i++){
             
-            for (let j=0; j < letter[i].length; j++){
-                if(letter[i][j].toLowerCase() === x) {
-                    currentWordVisual = currentWordVisual + letter[i][j] + ' ';
-                } else {
-                    currentWordVisual = currentWordVisual + '_ ';
-                }
+            if(currentGuess[i].toLowerCase() === x || lettersGuessed.indexOf(word.generate[i]) > -1 ) {
+                currentWordVisual = currentWordVisual + currentGuess[i] + ' ';
+                
+            } else {
+                currentWordVisual = currentWordVisual + '_ ';
             }
+        
         }
+
+        this.delete(x);
+        lettersGuessed.push(x);
+        currentWord = currentWordVisual;
+        
+        console.log(currentWordVisual); //test
     
     }
     
@@ -32,7 +44,7 @@ let word = {
 document.onkeyup = function(event) {
 
     let keyPress = String.fromCharCode(event.keyCode).toLowerCase();
-    if (letters.indexOf(keyPress) > -1) {
+    if (unguessedLetters.indexOf(keyPress) > -1) {
         console.log(keyPress); //test
         console.log(word.generate); //test
 
@@ -49,13 +61,18 @@ document.onkeyup = function(event) {
     } else {
         alert('Invalid Input');
     };
+
+    document.getElementById('wins').innerHTML= wins;
+    document.getElementById('currentWord').innerHTML = currentWord;
+    document.getElementById('guessesRemaining').innerHTML= guessesRemaining;
+    document.getElementById('lettersGuessed').innerHTML = lettersGuessed;
 }
 
 
 
 
 document.getElementById('wins').innerHTML= wins;
-document.getElementById('currentWord').innerHTML = currentWordVisual;
+document.getElementById('currentWord').innerHTML = currentWord;
 document.getElementById('guessesRemaining').innerHTML= guessesRemaining;
 document.getElementById('lettersGuessed').innerHTML = lettersGuessed;
 
