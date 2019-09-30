286a5a6e40fa74dcdf67b49f8263231f4b56460b
David <DMcCulloch86@gmail.com>
2019-09-30 16:54:50 -0400

    display values as letters are guessed correctly, need to debug check win

--- assets/javascript/game-2.js
+++ assets/javascript/game-2.js
@@ -4,12 +4,12 @@ let selectedWord = '';
 let arrayForSelectedWord = [];
 let selectedWordVisual = [];
 let incorrectLetters = [];
-let currentWordVisual = [];
 let unguessedLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 let wins = 0;
 let guessesRemaining = 10;
 
+//functions
 function reset() {
     //reset values for new word (made it into an array / make new array of _ to fill in for word in visual)
     selectedWord = wordBank [Math.floor(Math.random()*wordBank.length)];
@@ -21,6 +21,7 @@ function reset() {
     guessesRemaining = 10;
     incorrectLetters = [];
     selectedWordVisual = [];
+    unguessedLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
     //made 
     for (let i = 0; i < arrayForSelectedWord.length; i++){
@@ -30,29 +31,66 @@ function reset() {
     console.log (selectedWordVisual) //test
 }
 
-function displayValues () {
+function displayValues() {
     document.getElementById('wins').innerHTML = wins;
-    document.getElementById('currentWord').innerHTML = currentWordVisual;
+    document.getElementById('currentWord').innerHTML = selectedWordVisual;
     document.getElementById('guessesRemaining').innerHTML =  guessesRemaining;
     document.getElementById('lettersGuessed').innerHTML = incorrectLetters;
 
 }
 
+function checkLose() {
+    if(guessesRemaining == 0) {
+        alert ('You lose!')
+        displayValues();
+        reset();
+    }
+}
+
+function checkWin () {
+    if(selectedWordVisual == arrayForSelectedWord) {
+        alert ('You Won!')
+        displayValues();
+        reset();
+    }
+}
+
+//game running
 displayValues();
 reset();
 
-document.onkeyup (event) {
-    displayValues();
+document.onkeyup =  function(event) {
 
+    displayValues();
+    //set key pressed to var keyPress
     let keyPress = String.fromCharCode(event.keyCode).toLowerCase();
     
     if (unguessedLetters.indexOf(keyPress) > -1) {
-        //if they keyed in a number
-
-
-
+        //if they keyed is a letter and unguessed
 
         
+        //if the key is in the word
+        if(arrayForSelectedWord.indexOf(keyPress) > -1) {
+            console.log('correct letter') //test
+            for(i=0; i<arrayForSelectedWord.length; i++) {
+                //loop through array
+                if(arrayForSelectedWord[i] == keyPress) {
+                    selectedWordVisual[i] = arrayForSelectedWord[i];
+                    console.log(arrayForSelectedWord);
+                    console.log(selectedWordVisual);
+                }
+            }
+
+            displayValues();
+        } else {
+            //if the key is a letter, but not in word
+            guessesRemaining--;
+            checkLose ();
+            displayValues();
+        }
+
+
+
     } else {
         //if they keyed in a non-letter or a guessed letter
         alert('Input Error!!!')
